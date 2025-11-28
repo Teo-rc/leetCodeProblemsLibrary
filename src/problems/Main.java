c24c0faff57ad6ea144cbd40507a45ff751becac
Teo Ryan-Castillo <teoryan-castillo@hastingsdirect.com>
2025-11-28 11:30:04 +0000

    subarray daily challenge

--- src/problems/Main.java
+++ src/problems/Main.java
@@ -1,32 +1,46 @@
 package problems;
 
+import problems.arrays.sums.SubarraySumWithLengthCondition;
 import problems.concurrency.Foo;
 import problems.concurrency.FooBar;
 import problems.concurrency.ZeroEvenOdd;
 
+
 public class Main {
+    private static final boolean runConcurrency = false;
+    private static final boolean runArrays = true;
 
     public static void main(String[] args) throws InterruptedException {
 
-        // Initialise the test Foo object
-        System.out.println("Starting foo test case execution");
-        var fooTestCase = new int[]{2, 0, 1};
-        var foo = new Foo();
-        foo.fooTest(fooTestCase);
+        if (runConcurrency) {
+            // Initialise the test Foo object
+            System.out.println("Starting foo test case execution");
+            var fooTestCase = new int[]{2, 0, 1};
+            var foo = new Foo();
+            foo.fooTest(fooTestCase);
 
 
-        System.out.println();
-        System.out.println("Starting fooBar test case execution");
-        // Initialise the test FooBar object
-        var fooBarTestCase = 3;
-        var fooBar = new FooBar(fooBarTestCase);
-        fooBar.foBarTest();
+            System.out.println();
+            System.out.println("Starting fooBar test case execution");
+            // Initialise the test FooBar object
+            var fooBarTestCase = 3;
+            var fooBar = new FooBar(fooBarTestCase);
+            fooBar.foBarTest();
 
-        // Initialise the zero odd even test
-        System.out.println();
-        System.out.println("Starting Zero Even Odd test case execution");
-        var zeroEvenOdd = 90;
-        var zeroEven = new ZeroEvenOdd(zeroEvenOdd);
-        zeroEven.zeroEvenOddTest(zeroEvenOdd);
+            // Initialise the zero odd even test
+            System.out.println();
+            System.out.println("Starting Zero Even Odd test case execution");
+            var zeroEvenOdd = 90;
+            var zeroEven = new ZeroEvenOdd(zeroEvenOdd);
+            zeroEven.zeroEvenOddTest(zeroEvenOdd);
+        }
+        if (runArrays) {
+            System.out.println();
+            System.out.println("Starting Max Subarray Sum test case execution");
+
+            int[] nums = {-17,17,6};
+            int k = 1;
+            SubarraySumWithLengthCondition.testMaxSubarraySum(nums, k);
+        }
     }
 }
--- src/problems/arrays/sums/SubarraySumWithLengthCondition.java
+++ src/problems/arrays/sums/SubarraySumWithLengthCondition.java
@@ -1,27 +1,39 @@
 package problems.arrays.sums;
 
-import java.util.Arrays;
-
 public class SubarraySumWithLengthCondition {
 
-    public static long maxSubarraySum(int[] nums, int k) {
-        Arrays.sort(nums);
+    private static long maxSubarraySum(int[] nums, int k) {
 
-        var total = 0;
+        var incrementalTotals = new int[(nums.length % k) + 1][nums.length / k];
         var kthTotal = 0;
-        var index = nums.length - 1;
-        var endIndex = nums.length % k;
-
-        while (total + kthTotal >= total) {
-            kthTotal += nums[index];
-            if ((index - endIndex) % k == 0) {
-                total += kthTotal;
-                if (index == endIndex) {
-                    break;
+        for (int offset = 0; offset < (nums.length % k) + 1; offset++) {
+            kthTotal = 0;
+            for (int i = offset; i < nums.length; i++) {
+                kthTotal = kthTotal + nums[i];
+                if ((i - offset +1) % k == 0) {
+                    incrementalTotals[offset][(i - offset + 1)/k - 1] = kthTotal;
                 }
             }
-            index--;
         }
-        return total;
+        // max: total, start index, end index
+        var max = -999999999;
+        for (int offset = 0; offset < (nums.length % k) + 1; offset++) {
+            for (int x = 0; x < nums.length/k; x++) {
+                if (x != 0) {
+                    
+                }
+                if (incrementalTotals[offset][x] > max) {
+                    max = incrementalTotals[offset][x];
+                }
+                if (x != 0 && incrementalTotals[offset][x] - incrementalTotals[offset][x-1] > max) {
+                    max = incrementalTotals[offset][x] - incrementalTotals[offset][x-1];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void testMaxSubarraySum(int[] nums, int k) {
+        System.out.println(maxSubarraySum(nums, k));
     }
 }
